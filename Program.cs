3a9a4c2cfbde7056473ffea66868a45fb81997b9
Castro0109 <gustavocastro0109@gmail.com>
2025-04-07 21:15:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class Program{
+    static void Main(string[] args){
+
+        Console.WriteLine("Digite um num: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite outro num: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if(num1 > num2){
+            Console.WriteLine(num1 + ">" + num2);
+        }else{
+            Console.WriteLine(num2 + " > " + num1);
+        }
+
+        }
+}
